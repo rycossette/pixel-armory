20f8798603086f23133e60881b46bb05d9ce168d
rycossette <rycossette@gmail.com>
2024-07-01 11:20:18 -0500

    tweaked nav

--- components/Nav.js
+++ components/Nav.js
@@ -1,13 +1,20 @@
 import { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 const Nav = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const router = useRouter();
+  const currentPath = router.pathname;
+
+  const linkClasses = (path) =>
+    currentPath === path ? 'text-amber-500' : 'text-white hover:text-indigo-500';
 
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50 flex items-center justify-between w-full bg-black px-5 py-5">
-      <div className="flex items-center h-12">
+    <nav className="fixed top-0 left-0 right-0 z-50 flex items-center justify-center w-full bg-black px-5 py-10">
+      {/* Logo */}
+      <div className="absolute left-4 top-1/2 transform -translate-y-1/2 flex items-center h-12">
         <Image 
           src="/images/logo/pa.png" 
           width={100} 
@@ -16,14 +23,28 @@ const Nav = () => {
           className="h-full" 
         />
       </div>
-      <div className="hidden md:flex flex-grow justify-center">
-        <Link href="/" legacyBehavior><a className="text-white hover:text-indigo-500 active:text-amber-500 mx-4">Home</a></Link>
-        <Link href="/about" legacyBehavior><a className="text-white hover:text-indigo-500 active:text-amber-500 mx-4">About Us</a></Link>
-        <Link href="/gallery" legacyBehavior><a className="text-white hover:text-indigo-500 active:text-amber-500 mx-4">Showcase</a></Link>
-        <Link href="/services" legacyBehavior><a className="text-white hover:text-indigo-500 active:text-amber-500 mx-4">Services</a></Link>
-        <Link href="/contact" legacyBehavior><a className="text-white hover:text-indigo-500 mx-4">Contact</a></Link>
+
+      {/* Navigation Links */}
+      <div className="hidden md:flex justify-center flex-grow">
+        <Link href="/" legacyBehavior>
+          <a className={`${linkClasses('/')} mx-4`}>Home</a>
+        </Link>
+        <Link href="/about" legacyBehavior>
+          <a className={`${linkClasses('/about')} mx-4`}>About Us</a>
+        </Link>
+        <Link href="/gallery" legacyBehavior>
+          <a className={`${linkClasses('/gallery')} mx-4`}>Showcase</a>
+        </Link>
+        <Link href="/services" legacyBehavior>
+          <a className={`${linkClasses('/services')} mx-4`}>Services</a>
+        </Link>
+        <Link href="/contact" legacyBehavior>
+          <a className={`${linkClasses('/contact')} mx-4`}>Contact</a>
+        </Link>
       </div>
-      <div className="md:hidden flex items-center">
+
+      {/* Mobile Menu Button */}
+      <div className="md:hidden flex items-center absolute right-4 top-1/2 transform -translate-y-1/2">
         <button
           onClick={() => setIsOpen(!isOpen)}
           className="text-white focus:outline-none"
@@ -53,13 +74,24 @@ const Nav = () => {
           </svg>
         </button>
       </div>
+
       {isOpen && (
         <div className="absolute top-16 left-0 right-0 bg-gradient-to-l from-indigo-950 via-black to-indigo-950 md:hidden flex flex-col items-center space-y-4 py-4">
-          <Link href="/" legacyBehavior><a className="text-white hover:text-blue-500">Home</a></Link>
-          <Link href="/about" legacyBehavior><a className="text-white hover:text-blue-500">About Us</a></Link>
-          <Link href="/gallery" legacyBehavior><a className="text-white hover:text-blue-500">Showcase</a></Link>
-          <Link href="/services" legacyBehavior><a className="text-white hover:text-blue-500">Services</a></Link>
-          <Link href="/contact" legacyBehavior><a className="text-white hover:text-blue-500">Contact</a></Link>
+          <Link href="/" legacyBehavior>
+            <a className="text-white hover:text-blue-500">Home</a>
+          </Link>
+          <Link href="/about" legacyBehavior>
+            <a className="text-white hover:text-blue-500">About Us</a>
+          </Link>
+          <Link href="/gallery" legacyBehavior>
+            <a className="text-white hover:text-blue-500">Showcase</a>
+          </Link>
+          <Link href="/services" legacyBehavior>
+            <a className="text-white hover:text-blue-500">Services</a>
+          </Link>
+          <Link href="/contact" legacyBehavior>
+            <a className="text-white hover:text-blue-500">Contact</a>
+          </Link>
         </div>
       )}
     </nav>
